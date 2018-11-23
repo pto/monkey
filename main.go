8ad3fd91c9b65460fad398d0d79dd2c1f3137e90
Peter Olsen <pto@me.com>
2018-11-23 07:58:54 -0500

    thru ch. 1

---
+++ main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"os/user"
+
+	"github.com/pto/monkey/repl"
+)
+
+func main() {
+	user, err := user.Current()
+	if err != nil {
+		panic(err)
+	}
+	name := user.Name
+	if name == "" {
+		name = user.Username
+	}
+	fmt.Printf("Hello, %s! This is the Monkey programming language.\n", name)
+	repl.Start(os.Stdin, os.Stdout)
+}
